ac354e0f07cc365c1bcf69bf3e2355e2534ba4cc
VictorMattosOW <victor_rm@outlook.com>
2023-06-16 17:08:04 -0300

    create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import { HeaderContainer, MapLocale, ShoppingCar } from './styles'
+import Logo from '../../assets/Logo.png'
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
 export function Header() {
-  return <h1>HEADER</h1>
+  return (
+    <HeaderContainer>
+      <section>
+        <img src={Logo} alt="Logo Coffee Delivery" />
+      </section>
+      <nav>
+        <MapLocale>
+          <MapPin color="#8047F8" weight="fill" size={19} />
+          <p>Porto Alegre, RS</p>
+        </MapLocale>
+        <ShoppingCar>
+          <ShoppingCart color="#C47F17" weight="fill" size={22} />
+        </ShoppingCar>
+      </nav>
+    </HeaderContainer>
+  )
 }
